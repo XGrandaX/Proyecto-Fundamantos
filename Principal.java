d7f9d1485a3308bc1abd64aa4d88985968b12a70
XGrandaX <93231680+XGrandaX@users.noreply.github.com>
2021-11-24 12:14:07 -0500

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,10 +1,12 @@
-package fundamentos;
-
+import java.io.IOException;
+import java.sql.SQLOutput;
+import java.time.LocalTime;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Principal {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         boolean todo = true;
         Scanner sc = new Scanner(System.in);
         String placa;
@@ -18,6 +20,10 @@ public class Principal {
         int es1;
         int ps;
         int es;
+        String tipo;
+        int precioCarro;
+        int precioMoto;
+        int tiempoOcupado;
 
         System.out.println("Ingrese la cantidad de pisos");
         int pisos = sc.nextInt();
@@ -38,6 +44,10 @@ public class Principal {
 
             }
         }
+        System.out.println("Ingrese precio parqueadero carros: ");
+        precioCarro = sc.nextInt();
+        System.out.println("Ingrese precio parqueadero motos: ");
+        precioMoto = sc.nextInt();
 
         while (todo) {
             System.out.println("Ingrese un numero:");
@@ -54,39 +64,98 @@ public class Principal {
                     System.out.println("Piso y Espacio donde desee parquear:");
                     ps = sc.nextInt();
                     es = sc.nextInt();
-                    if (Sensor.sensores[ps][es].getEstado() == 0) {
-                        placa = sc.next();
-                        marca = sc.next();
-                        color = sc.next();
-                        Vehiculo vehiculos2 = new Vehiculo(placa, marca, color);
-                        Vehiculo.vehiculos[ps][es] = vehiculos2;
-                        Sensor.sensores[ps][es].setEstado(1);
+                    System.out.println("Ingrese tipo de vehículo:");
+                    tipo = sc.next();
+                    if (tipo.equalsIgnoreCase("moto")) {
+                        if (Sensor.sensores[ps][es].getEstado() == 0) {
+                            System.out.println("Ingrese placa");
+                            placa = sc.next();
+                            System.out.println("Ingrese marca ");
+                            marca = sc.next();
+                            System.out.println("Ingrese color ");
+                            color = sc.next();
+                            System.out.println("Ingrese valor comercial ");
+                            valor = sc.nextInt();
+                            LocalTime horaIngreso = LocalTime.now();
+                            Vehiculo vehiculos2 = new Moto(placa, marca, color, valor, horaIngreso, precioMoto);
+                            Vehiculo.vehiculos[ps][es] = vehiculos2;
+                            Sensor.sensores[ps][es].setEstado(1);
 
-                    } else {
-                        System.out.println("No se pudo realisar la accion.");
+                        } else {
+                            System.out.println("No se pudo realizar la accion.");
+                        }
+                    }
+                    if (tipo.equalsIgnoreCase("carro")) {
+                        if (Sensor.sensores[ps][es].getEstado() == 0) {
+                            System.out.println("Ingrese placa");
+                            placa = sc.next();
+                            System.out.println("Ingrese marca ");
+                            marca = sc.next();
+                            System.out.println("Ingrese color ");
+                            color = sc.next();
+                            System.out.println("Ingrese valor comercial ");
+                            valor = sc.nextInt();
+                            LocalTime horaIngreso = LocalTime.now();
+                            Vehiculo vehiculos2 = new Carro(placa, marca, color, valor, horaIngreso, precioCarro);
+                            Vehiculo.vehiculos[ps][es] = vehiculos2;
+                            Sensor.sensores[ps][es].setEstado(1);
+
+                        } else {
+                            System.out.println("No se pudo realizar la accion.");
+                        }
                     }
                     break;
                 case 3:
                     System.out.println("Piso y Espacio donde desee parquear:");
-                    ps1 = sc.nextInt();
-                    es1 = sc.nextInt();
-                    if (Sensor.sensores[ps1][es1].getEstado() == 0) {
-                        placa = sc.next();
-                        marca = sc.next();
-                        color = sc.next();
-                        valor = sc.nextInt();
-                        Vehiculo vehiculos2 = new Vehiculo(placa, marca, color, valor);
-                        Vehiculo.vehiculos[ps1][es1] = vehiculos2;
-                        Sensor.sensores[ps1][es1].setEstado(1);
+                    ps = sc.nextInt();
+                    es = sc.nextInt();
+                    System.out.println("Ingrese tipo de vehículo:");
+                    tipo = sc.next();
+                    if (tipo.equalsIgnoreCase("moto")) {
+                        if (Sensor.sensores[ps][es].getEstado() == 0) {
+                            System.out.println("Ingrese placa");
+                            placa = sc.next();
+                            System.out.println("Ingrese marca ");
+                            marca = sc.next();
+                            System.out.println("Ingrese color ");
+                            color = sc.next();
+                            System.out.println("Ingrese valor comercial ");
+                            valor = sc.nextInt();
+                            LocalTime horaIngreso = LocalTime.now();
+                            Vehiculo vehiculos2 = new Moto(placa, marca, color, valor, horaIngreso, precioMoto);
+                            Vehiculo.vehiculos[ps][es] = vehiculos2;
+                            Sensor.sensores[ps][es].setEstado(1);
 
-                    } else {
-                        System.out.println("No se pudo realisar la accion.");
+                        } else {
+                            System.out.println("No se pudo realizar la accion.");
+                        }
+                    }
+                    if (tipo.equalsIgnoreCase("carro")) {
+                        if (Sensor.sensores[ps][es].getEstado() == 0) {
+                            System.out.println("Ingrese placa");
+                            placa = sc.next();
+                            System.out.println("Ingrese marca ");
+                            marca = sc.next();
+                            System.out.println("Ingrese color ");
+                            color = sc.next();
+                            System.out.println("Ingrese valor comercial ");
+                            valor = sc.nextInt();
+                            LocalTime horaIngreso = LocalTime.now();
+                            Vehiculo vehiculos2 = new Carro(placa, marca, color, valor, horaIngreso, precioCarro);
+                            Vehiculo.vehiculos[ps][es] = vehiculos2;
+                            Sensor.sensores[ps][es].setEstado(1);
+                            System.out.println(Vehiculo.vehiculos[ps][es].toString());
+                        } else {
+                            System.out.println("No se pudo realizar la accion.");
+                        }
                     }
                     break;
                 case 4:
+                    System.out.println("Vehiculos almacenados: ");
                     System.out.println(Vehiculo.VehiculosCompletos());
                     break;
                 case 5:
+                    System.out.println("Cantidad de vehículos creados: ");
                     System.out.println(Vehiculo.VhCantidad(vehiculos));
                     break;
                 case 6:
@@ -111,6 +180,21 @@ public class Principal {
 
                         }
                     }
+                    break;
+                case 10:
+                    System.out.println("Ingrese piso del sensor a desactivar");
+                    ps = sc.nextInt();
+                    System.out.println("Ingrese estacionamiento del sensor a desactivar");
+                    es = sc.nextInt();
+                    System.out.println("Ingrese tiempo ocupado en el parquedero: ");
+                    tiempoOcupado = sc.nextInt();
+                    System.out.println(Vehiculo.calcularPrecio(ps, es, tiempoOcupado));
+                    System.out.println("El vehículo ha salido eliminado del parqueadero.");
+                    Vehiculo.desactivarSensor(ps, es);
+                    break;
+                case 11:
+                    Vehiculo.exportarVehiculos();
+                    System.out.println("La información de los vehículos ha sido almacenada.");
                     break;
                 default:
                     System.out.println("Comando incorrecto");
