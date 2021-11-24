de6d9b5acc72585c0936853e31b2dfb415077b5c
XGrandaX <93231680+XGrandaX@users.noreply.github.com>
2021-11-24 12:14:54 -0500

    Update Sensor.java

--- Sensor.java
+++ Sensor.java
@@ -1,12 +1,10 @@
-package fundamentos;
-
 public class Sensor {
     private int estado = 0;
     public static Sensor[][] sensores;
     public Sensor (){
-        
+
     }
-    
+
     public Sensor(int e){
         this.estado = e;
     }
@@ -30,34 +28,34 @@ public class Sensor {
     public static void setSensores(Sensor[][] sensores) {
         Sensor.sensores = sensores;
     }
-     public static String sensorLibre (Sensor sensores[][]){
-         String nada ="";
-         for(int i = 0; i < sensores.length; i++){
-             for(int j = 0; j < sensores[i].length; j++){
-                 if(sensores[i][j] != null && sensores[i][j].getEstado() == 0){
-                     String dados ="piso"+i +"espacio"+j;
-                     nada = nada + dados;
-                     
-                 }
-             }
-         } return nada;
-     }
-     
-     public static String sensoresEstado (Sensor[][] sensores){
-         String nada ="";
-         for(int i = 0; i < sensores.length; i++){
-             for(int j = 0; j < sensores[i].length; j++){
-                 if(sensores[i][j] != null){
-                     String hola ="piso"+i +"espacio"+j + sensores[i][j].getEstado()+" ";
-                     nada = nada + hola; 
-                     
-                 }
-             }
-         } return nada;
-     }
-     
-    
-   
-    
+    public static String sensorLibre (Sensor sensores[][]){
+        String nada ="";
+        for(int i = 0; i < sensores.length; i++){
+            for(int j = 0; j < sensores[i].length; j++){
+                if(sensores[i][j] != null && sensores[i][j].getEstado() == 0){
+                    String dados =" piso "+i +" espacio "+j;
+                    nada = nada + dados;
+
+                }
+            }
+        } return nada;
+    }
+
+    public static String sensoresEstado (Sensor[][] sensores){
+        String nada ="";
+        for(int i = 0; i < sensores.length; i++){
+            for(int j = 0; j < sensores[i].length; j++){
+                if(sensores[i][j] != null){
+                    String hola ="piso"+i +"espacio"+j + sensores[i][j].getEstado()+" ";
+                    nada = nada + hola;
+
+                }
+            }
+        } return nada;
+    }
+
+
+
+
 }
 
